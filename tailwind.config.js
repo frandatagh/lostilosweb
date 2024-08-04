ba7058c35a6b79084f017aa87c917dcdd620a723
Fran Ruiz <francoruizgabriel@gmail.com>
2024-08-04 19:21:14 -0300

    Agregando paleta de colores

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark': '#474843',
+        'custom-green': '#10B981',
+        'custom-red': '#EF4444',
+        'custom-yellow': '#F59E0B',
+      }
+    },
+  },
+  plugins: [],
+}
+
